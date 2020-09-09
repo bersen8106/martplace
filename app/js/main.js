1a1e2fe315c22fb37467517990830dd4b38fc27c
bersen8106 <bersen8106@gmail.com>
2020-09-09 14:16:39 +0500

    added weekly product slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
+    $('.rate-star').rateYo({
+        rating: 4.5,
+        starWidth: '20px',
+        normalFill: "#fff",
+        spacing: "5px",
+        readOnly: true,
+    });
 
+
+
+    $('.weekly-product-slider').slick({
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/icons/prew-arrow.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/icons/next-arrow.svg" alt=""></img>',
+    });
 
 });
