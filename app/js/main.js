78c937fb5ced4fde2012885c3aa49c0184fb95ac
bersen8106 <bersen8106@gmail.com>
2020-09-15 21:22:52 +0500

    added total & choose martplace section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,7 @@
 $(function() {
 
+    var mixer = mixitup('.cards__inner');
+
     $('.rate-stars').rateYo({
         rating: 4.5,
         spacing: "5px",
@@ -11,23 +13,19 @@ $(function() {
         normalFill: "transparent",
     });
 
-    $('.rate-stars, .mix').rateYo({
-        rating: 4.5,
-        spacing: "5px",
-        maxValue: 5,
-        numStars: 5,
-        starWidth: '19px',
-        readOnly: true,
-        ratedFill: '#F39C12',
-        normalFill: "transparent",
-    });
-
-
-
     $('.product-slider').slick({
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/icons/prew-arrow.svg" alt=""></img>',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/icons/next-arrow.svg" alt=""></img>',
     });
+    $('.feed__inner').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        variableWidth: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/icons/prew-arrow.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/icons/next-arrow.svg" alt=""></img>',
+    });
+
 
 });
